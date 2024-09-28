fa56b513e8128b35613677fed91f2f9854b80fcb
Ivan Milic <milic.tg@gmail.com>
2024-09-28 09:46:33 +0200

    routes in progress

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        //don't use this
+        $middleware->validateCsrfTokens(except: ['/submit']);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/jobs', function () {
+    return 'Jobs';
+});
+
+Route::post('/submit', function () {
+    return 'Submitted';
+});
+
+Route::get(('/posts/{id}'), function (string $id) {
+    return 'Post ' . $id;
+});
